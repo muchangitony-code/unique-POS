176b0eec7bbf40fdd10b6062e8c47b7db6337646
muchangitony-code <muchangi.tony@gmail.com>
2026-08-14 11:55:57 +0300

    Allow quotations to contain non-stock custom line items

---
+++ migrations/0006_quotation_custom_items.sql
@@ -0,0 +1,8 @@
+BEGIN;
+
+-- Quotations may contain items that are not part of inventory.
+-- Existing inventory-linked quotation lines continue to use product_id.
+ALTER TABLE public.quotation_items
+  ALTER COLUMN product_id DROP NOT NULL;
+
+COMMIT;
